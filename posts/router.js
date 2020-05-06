114bc5184df6dc6ad0a4f673c2c4a63de3ab3d9b
j721 <juliadvu@gmail.com>
2020-05-06 00:07:37 -0500

    trying another way to create delete request

--- posts/router.js
+++ posts/router.js
@@ -99,6 +99,20 @@ router.delete("/:id", (req, res)=>{
   })
 })
 
+// router.delete('/:id', (req, res)=>{
+//   const { id } = req.params;
+//   Posts.remove(id)
+//   .then(post=>{
+//     if(post){
+//       res.status(200).json( post)
+//     }else{
+//       res.status(404).json({message: "The post with the specified ID does not exist."})
+//     }
+//   }).catch((err)=>{
+//     res.status(500).json({error:"The post could not be removed."})
+//   })
+// })
+
 
 //PUT request to /api/posts/:id:
 
