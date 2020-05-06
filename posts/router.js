b6337918abdab711b7eb0f6037e882477dec6f7f
j721 <juliadvu@gmail.com>
2020-05-05 23:56:48 -0500

    working on post requests

--- posts/router.js
+++ posts/router.js
@@ -1,15 +1,42 @@
 const express = require("express");
-
 const Posts = require("../data/db.js");
-
 const router = express.Router(); 
 
-
 // POST request to /api/posts
-
 router.post("/", (req, res)=>{
-  Posts.find()
+const { title, contents } = req.body;
+
+if(!title || !contents){
+  res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+}else {
+  Posts.insert(req.body)
+  .then((posts)=>{
+    res.status(201).json(posts)
+  }).catch((err)=>{
+    res.status(500).json({error: "There was an error while saving the post to the database."})
+  })
+}
 })
+
+
+//POST request to /api/posts/:id/comments
+router.post('/:id/comments', (req, res)=>{
+  const { id } = req.params;
+  Posts.findById(id)
+  .then((post)=>{
+    if (!post.length){
+      res.status(404).json({errorMessage: "The post with the specified ID does not exist."})
+    }else if(!req.body.text){
+        res.status(400).json({errorMessage: "Please provide text for the comment."})
+    }else{
+      Posts.insertComment(req.body)
+      .then((comment)=>{
+        res.status(500).json({errorMessage:"There was an error while saving the comment to the database."})
+      })
+    }
+  })  
+})
+
 
 //GET  /api/posts Returns an array of all post objects saved in database.
 router.get("/", (req, res) => {
